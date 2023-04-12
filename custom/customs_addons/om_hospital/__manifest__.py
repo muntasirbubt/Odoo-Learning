70033938cedde9cd7b0735c396bcf016c255be13
MD-Muntasir-Kamal <muntasir3217@gmail.com>
2023-04-12 14:27:29 +0600

    load data from file

--- custom/customs_addons/om_hospital/__manifest__.py
+++ custom/customs_addons/om_hospital/__manifest__.py
@@ -15,6 +15,7 @@
     'depends': ['mail', 'product'],
     'data': [
         'security/ir.model.access.csv',
+        'data/patient_tag_data.xml',
         'wizard/cancel_appointment_view.xml',
         'views/menu.xml',
         'views/patient_view.xml',
